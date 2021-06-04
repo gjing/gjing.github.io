a3b034e41230cf837d89cd2ff0397111e4688e58
George Jing <georgejing93@gmail.com>
2021-06-04 00:41:15 -0700

    fixed typo and added new banner

--- static/header.js
+++ static/header.js
@@ -1,11 +1,18 @@
-const header = "Welcome to George Jing's Website\n\n\
+const header = "WELCOME TO GEORGE JING'S WEBSITE\n\n\
+\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2584}\u{2584}\u{2500}\u{2500}\u{2500}\u{2590}\u{2588}\u{2500}\n\
+\u{2500}EVOLVING\u{2500}FROM\u{2500}\u{2500}\u{2500}\u{2500}\u{2584}\u{2584}\u{2584}\u{2500}\u{2500}\u{2500}\u{2584}\u{2588}\u{2588}\u{2584}\u{2500}\u{2500}\u{2588}\u{2580}\u{2500}\u{2500}\u{2500}\u{2588}\u{2500}\u{2584}\n\
+\u{2500}A\u{2500}CODE\u{2500}MONKEY\u{2500}\u{2500}\u{2584}\u{2588}\u{2588}\u{2580}\u{2588}\u{258C}\u{2500}\u{2588}\u{2588}\u{2584}\u{2584}\u{2500}\u{2500}\u{2590}\u{2588}\u{2580}\u{2584}\u{2500}\u{2590}\u{2588}\u{2580}\u{2500}\n\
+\u{2500}TO\u{2500}A\u{2500}CODE\u{2500}MAN\u{2500}\u{2590}\u{2588}\u{2580}\u{2580}\u{258C}\u{2500}\u{2500}\u{2500}\u{2584}\u{2580}\u{258C}\u{2500}\u{258C}\u{2500}\u{2588}\u{2500}\u{258C}\u{2500}\u{2500}\u{258C}\u{2500}\u{258C}\u{2500}\n\
+\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{2500}\u{258C}\u{2580}\u{2584}\u{2500}\u{2590}\u{2500}\u{2500}\u{2580}\u{2584}\u{2500}\u{2590}\u{2584}\u{2500}\u{2590}\u{2584}\u{2590}\u{2584}\u{2500}\u{2590}\u{2584}\u{2500}\u{2590}\u{2584}\n\n\
  * LinkedIn:   https://www.linkedin.com/in/george-jing-652b246a/\n\
  * Github:     https://github.com/gjing/\n\
  * Email:      George.Jing@dependentstudy.xyz\n\n\
-  Personal status information as of Fri Jun  4 05:16:55 UTC 2021\n\n\
+  Personal status as of Fri Jun  4 05:16:55 UTC 2021\n\n\
   Employment Status: Employed\n\
-  Currently seeking new work: Not at this moment\n\
-  Open to new oppurtunities: Not at this moment\n\n\
+  Legal Status: US Citizen\n\
+  Approximate Location: Southern California\n\
+  Currently seeking new work?: Not at this moment\n\
+  Open to new opportunities?: Not at this moment\n\n\
  * Experienced full stack developer with a bachelor's in science in\n\
    Physics from California Polytechnic State University-San Luis Obispo.\n\
    Primarily working with a Python/Django, Javascript, Html/CSS, and\n\
